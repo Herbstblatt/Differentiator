ec1c13a42a0f06dec10b258e184d7c50471cdb65
Herbstblatt <48344463+Herbstblatt@users.noreply.github.com>
2025-03-18 18:21:09 +0300

    Change defaults to double and add complex num support

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <sstream>
 
-std::istream &operator>>(std::istream &is, std::pair<std::string, int> &p) {
+std::istream &operator>>(std::istream &is, std::pair<std::string, double> &p) {
     std::stringbuf buf{};
     is.get(buf, '=');
     is.get();
@@ -17,13 +17,15 @@ int main(int argc, char** argv) {
     TCLAP::ValueArg<std::string> eval_arg("","eval","An expression to evaluate",false,"","expr");
     TCLAP::ValueArg<std::string> diff_arg("","diff","An expression to differentiate",false,"","expr");
     TCLAP::ValueArg<std::string> by_arg("","by","A variable to differentiate by",false,"","variable");
+    TCLAP::SwitchArg complex_arg("c", "complex", "Whether to allow complex numbers", false);
 
     TCLAP::OneOf input;
     input.add(eval_arg).add(diff_arg);
     cmd.add(input);
     cmd.add(by_arg);
+    cmd.add(complex_arg);
 
-    TCLAP::UnlabeledMultiArg<std::pair<std::string, int>> args("vars", "Variable values", false, "name=value");
+    TCLAP::UnlabeledMultiArg<std::pair<std::string, double>> args("vars", "Variable values", false, "name=value");
 
     cmd.add(args);
     cmd.parse(argc, argv);
@@ -33,21 +35,45 @@ int main(int argc, char** argv) {
             std::cerr << "ERROR: --by arg only allowed in differentiation mode. Run with -h to learn more." << std::endl;
             return 1;
         }
-        try {
-            expression::Expression<int> expr(eval_arg.getValue());
+        if (complex_arg.isSet()) {
+            try {
+                expression::Expression<std::complex<double>> expr(eval_arg.getValue());
 
-            std::vector<std::pair<std::string, int>> vars = args.getValue();
-            std::unordered_map<std::string, int> var_map(vars.begin(), vars.end());
+                std::vector<std::pair<std::string, double>> vars = args.getValue();
+                std::unordered_map<std::string, std::complex<double>> var_map;
+                for (const auto& [c, v] : vars) {
+                    var_map[c] = std::complex<double>(v, 0);
+                }
 
-            int result = expr.evaluate(var_map);
-            std::cout << result << std::endl;
-        } catch (const expression::ParseError &e) {
-            std::cerr << "ERROR when parsing the expression: " << e.what() << std::endl;
-            return 1;
-        } catch (const expression::SymbolsUnspecified &e) {
-            std::cerr << "ERROR during evaluation: " << e.what() << std::endl;
-            return 1;
+
+                expr.substitute("i", std::complex(0.0,1.0));
+                std::complex<double> result = expr.evaluate(var_map);
+                std::cout << result << std::endl;
+            } catch (const expression::ParseError &e) {
+                std::cerr << "ERROR when parsing the expression: " << e.what() << std::endl;
+                return 1;
+            } catch (const expression::SymbolsUnspecified &e) {
+                std::cerr << "ERROR during evaluation: " << e.what() << std::endl;
+                return 1;
+            }
+        } else {
+            try {
+                expression::Expression<double> expr(eval_arg.getValue());
+
+                std::vector<std::pair<std::string, double>> vars = args.getValue();
+                std::unordered_map<std::string, double> var_map(vars.begin(), vars.end());
+
+                double result = expr.evaluate(var_map);
+                std::cout << result << std::endl;
+            } catch (const expression::ParseError &e) {
+                std::cerr << "ERROR when parsing the expression: " << e.what() << std::endl;
+                return 1;
+            } catch (const expression::SymbolsUnspecified &e) {
+                std::cerr << "ERROR during evaluation: " << e.what() << std::endl;
+                return 1;
+            }
         }
+
     }
     else if (diff_arg.isSet()) {
         if (!by_arg.isSet()) {
@@ -55,9 +81,9 @@ int main(int argc, char** argv) {
             return 1;
         }
         try {
-            expression::Expression<int> expr(diff_arg.getValue());
+            expression::Expression<double> expr(diff_arg.getValue());
 
-            std::vector<std::pair<std::string, int>> vars = args.getValue();
+            std::vector<std::pair<std::string, double>> vars = args.getValue();
             for (const auto& [name, value] : vars) {
                 expr.substitute(name, value);
             }
